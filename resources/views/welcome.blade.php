b259f5f4b867ba1acb89a1b993b16123e5302ad7
Zubayer Hossain <zubayer94@gmail.com>
2021-07-05 10:45:25 +0600

    Initial setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Mediusware</title>
+    {{-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/logo/edusms_favicon.png') }}" /> --}}
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
+    <script src="{{ asset('js/app.js') }}" defer ></script>
+  
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
